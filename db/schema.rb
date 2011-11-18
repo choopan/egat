061d8489ca540585e33bc336cbb54a61142fe645
Choopan Rattanapoka <choopanr@hotmail.com>
2011-11-18 07:16:19 +0700

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111110073005) do
+ActiveRecord::Schema.define(:version => 20111111014857) do
 
   create_table "aging_product_factors", :force => true do |t|
     t.integer  "hi_factor"
@@ -1054,8 +1054,8 @@ ActiveRecord::Schema.define(:version => 20111110073005) do
     t.string   "environment"
     t.string   "failurestatus"
     t.string   "failuredetail"
-    t.datetime "downdate"
-    t.datetime "update"
+    t.datetime "downdatetime"
+    t.datetime "updatetime"
     t.string   "workorder"
     t.string   "failuregroup"
     t.string   "failurepart"
